9a22d66c1d4b817e948ca13d6f2971420f9f6f06
Nathan <nathan.watson.172@gmail.com>
2024-06-03 18:39:16 +0100

    Added some stuff

--- newsletter-next/app/layout.tsx
+++ newsletter-next/app/layout.tsx
@@ -1,22 +1,16 @@
-import type { Metadata } from "next";
-import { Fredoka } from "next/font/google";
-import "./globals.css";
-
-const fredoka = Fredoka({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Newsletter",
-  description: "",
-};
-
+import '@/app/ui/global.css';
+import '@/app/ui/global-newsletters.css';
+ 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={fredoka.className}>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
